815148d2fd7469bcded8c86ba48dba3b95c18aa6
moon1710 <131950850+moon1710@users.noreply.github.com>
2025-05-09 00:54:59 -0600

    bingbong

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,10 +1,8 @@
+// Toggle inicial y al clic
 document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.getElementById("sidebar");
-  // Iniciar el sidebar colapsado
   sidebar.classList.add("collapsed");
-
-  // Toggle al hacer click en el botón
-  document.getElementById("expandBtn").addEventListener("click", () => {
-    sidebar.classList.toggle("collapsed");
-  });
+  document
+    .getElementById("expandBtn")
+    .addEventListener("click", () => sidebar.classList.toggle("collapsed"));
 });
